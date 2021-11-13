308fe07df86c86c9babc5eec00ace05d1ac60ac9
Tarun Aggarwal <48708852+Tarunagg1@users.noreply.github.com>
2021-11-13 13:13:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+//Load express module with `require` directive
+var express = require('express')
+var app = express()
+
+
+//Define request response in root URL (/)
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+// docker build -t node-app . for run
+
+//Launch listening server on port 8081
+app.listen(8081, function () {
+  console.log('app listening on port 8081!')
+})
